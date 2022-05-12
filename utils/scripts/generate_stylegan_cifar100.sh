858a65174a722136750590ff07aff622c7d42109
Seungwook Han <sh3264@coumbia.edu>
2022-05-12 15:00:21 -0400

    Added script for running cifar100 generation with sgan

---
+++ utils/scripts/generate_stylegan_cifar100.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+conda activate torch1.8
+
+python3 generate_dataset_stylegan.py \
+    --out_dir /disk_d/han/data/ \
+    --checkpoint_path /disk_d/han/data/stylegan_checkpoints/220000.pt \
+    --partition train \
+    --truncation 2.0 \
+    --batch_size 32 \
+    --num_imgs 500 \
+    --dataset_type cifar100 \
+    --num_neighbors 1 \
+    --std 0.2
