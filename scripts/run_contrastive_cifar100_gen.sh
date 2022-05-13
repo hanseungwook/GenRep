14c1d392730435757f43b0dc28d1c9d16a853bec
Seungwook Han <sh3264@coumbia.edu>
2022-05-13 13:23:08 -0400

    Fixed number of workers

--- scripts/run_contrastive_cifar100_gen.sh
+++ scripts/run_contrastive_cifar100_gen.sh
@@ -2,5 +2,5 @@
 
 CUDA_VISIBLE_DEVICES=0,1 python3 main_unified.py --method SimCLR --cosine \
 	--dataset gan --data_folder /disk_d/han/data/cifar100_generated/stylegan_cifar100_tr2.0_gauss1_std0.2_NS500_NN1 --walk_method my_gauss \
-    --batch_size 256 --learning_rate 0.03 --temp 0.5 --num_workers 64 \
+    --batch_size 256 --learning_rate 0.03 --temp 0.5 --num_workers 16 \
 	--cache_folder /disk_d/han/GenRep/results/gan_cifar100/ >> log_train_simclr_cifar100.txt &
